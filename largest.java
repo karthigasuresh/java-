7f9571503fbb6512404f8afa530aea2bc5827eae
Karthigasuresh <karthigasuresh08@gmail.com>
2026-08-15 22:03:38 +0530

    Add program to find the largest of two numbers

---
+++ largest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class largest{
+  public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    int number1=sc.nextInt();
+    int number2=sc.nextInt();
+    if(number1>number2){
+      System.out.println("Number1 is largest");
+    }
+    else{
+      System.out.println("Number2 is largest");
+    }
+  }
+}
+    
