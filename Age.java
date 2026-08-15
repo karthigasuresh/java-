848147b616c6f1bb8bf558a2d96808ebf3cdee4f
Karthigasuresh <karthigasuresh08@gmail.com>
2026-08-15 22:21:01 +0530

    Add Age class to check if user is an adult

---
+++ Age.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Age{
+  public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    int age=sc.nextInt();
+    if(age>=18){
+      System.out.println("YES");
+    }
+    else{
+      System.out.println("NO");
+    }
+  }
+}
