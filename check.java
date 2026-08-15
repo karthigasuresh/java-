aaebbd5323c5ee6fe94cdb36968ad6fa73cc71e6
Karthigasuresh <karthigasuresh08@gmail.com>
2026-08-15 21:52:49 +0530

    Create check.java

---
+++ check.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class check{
+  public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    int numbers=sc.nextInt();
+    if(n>=0){
+      System.out.println("Positive");
+    }
+    else{
+      System.out.println("Negative");
+    }
+  }
+}
