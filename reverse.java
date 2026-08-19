fcae2ea968806fc2879af9925e7747fa29c35cea
Karthigasuresh <karthigasuresh08@gmail.com>
2026-08-19 22:06:29 +0530

    Add reverse array functionality in reverse.java

---
+++ reverse.java
@@ -0,0 +1,18 @@
+public class reverse {
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        System.out.println("Original Array:");
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
+        System.out.println("\n\nReversed Array:");
+
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
