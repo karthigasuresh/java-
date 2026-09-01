9684e0fe11a0c5040a941d3f97f59cd2fdf17dfd
Karthigasuresh <karthigasuresh08@gmail.com>
2026-09-01 18:50:48 +0530

    to count characters in a string

---
+++ Count.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+
+        String str = "Welcome";
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            count++;
+        }
+
+        System.out.println("Number of characters = " + count);
+    }
+}
