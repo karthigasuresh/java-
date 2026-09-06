3913fd99b0cb5da083736772c79c38bef80ad7f1
Karthigasuresh <karthigasuresh08@gmail.com>
2026-09-06 23:09:49 +0530

    Add Java program to count vowels and consonants

---
+++ countvowelsandconsonant.java
@@ -0,0 +1,26 @@
+```java
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello Java".toLowerCase();
+
+        int vowels = 0;
+        int consonants = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                    ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
+```
