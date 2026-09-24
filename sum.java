375c7a687b88c59f90c2edf7a3b4ef7788cebb12
Karthigasuresh <karthigasuresh08@gmail.com>
2026-09-24 12:02:19 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,21 @@
+ ListNode dummy = new ListNode(0);
+        ListNode current=dummy;
+        int carry=0;
+        while(l1!=null ||l2!=null || carry!=0){
+            int sum=carry;
+            if(l1!=null){
+                sum=sum+l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                sum=sum+l2.val;
+                l2=l2.next;
+            }
+            carry=sum/10;
+            int rem=sum%10;
+            current.next=new ListNode(rem);
+            current=current.next;
+        }
+        return dummy.next;
+    }
+}
